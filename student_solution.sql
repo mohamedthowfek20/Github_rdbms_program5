f3f6d60a78e896f2a3938919560f3d705fa1c72f
MOHAMED THOWFEK P <thowfickthowfick03@gmail.com>
2026-09-09 10:27:56 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,11 @@
+INSERT INTO Departments(DepartmentID,DepartmentName)
+VALUES
+(102,'CS'),
+(104,'IT');
+SELECT*FROM Departments;
+INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
+VALUES(1001,'Arun','Male',101),
+(1002,'Divya','Female',102),
+(1003,'Karthik','Male',101);
+SELECT*FROM Student;
 
